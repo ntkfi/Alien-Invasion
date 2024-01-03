38ea822acd79c85671221a1ad001dc0f478f9bcc
ntkfi <nikokumpulainen@hotmail.fi>
2024-01-03 17:53:56 +0200

    character movement right added

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,18 +23,26 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
+            self.clock.tick(60)
+    
+    def _check_events(self):
+         """Respond to keypresses and mouse events."""
+         for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right
+                    self.ship.rect.x += 1
 
-            # Redraw the screen during each pass through the loop.
+    def _update_screen(self):
+            """Update images on the screen, and flip to the new screen."""
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
-            
-            # Make the most recently drawn screen visible.
+
             pygame.display.flip()
-            self.clock.tick(60)
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
