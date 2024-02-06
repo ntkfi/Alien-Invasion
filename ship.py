a1717631dbbac7acea51b97d53b0a18a7d020906
Kumpulainen Niko Tapani <ntkumpul@student.jyu.fi>
2024-02-06 21:20:31 +0200

    speed adjustment p244

--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class Ship:
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
@@ -15,16 +16,23 @@ class Ship:
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
-        # Movement flag; start with a ship that's not moving.
+        # Store a float for the ship's exact horizontal position.
+        self.x = float(self.rect.x)
+
+        # Movement flags; start with a ship that's not moving.
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Update the ship's position based on the movement flag."""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        # Update the ship's x value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        
+        # Update rect object from self.x.
+        self.rect.x = self.x
 
 
     def blitme(self):
