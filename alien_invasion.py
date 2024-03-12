eddcb32760b5deadd13804bb372dffbcb1c1d9de
ntkfi <ntkumpul@student.jyu.fi>
2024-03-12 18:24:32 +0200

    hide mouse cursor

--- alien_invasion.py
+++ alien_invasion.py
@@ -36,7 +36,7 @@ class AlienInvasion:
         self.game_active = False
 
         # Make the play button.
-        self.play_button = Button(self, "草泥马")
+        self.play_button = Button(self, "Play")
     
     def run_game(self):
         """Start the main loop for the game."""
@@ -71,7 +71,9 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
         elif event.key == pygame.K_SPACE:
-            self._fire_bullet()
+            # Prevent bullet firing while game is not running.
+            if self.game_active:
+                self._fire_bullet()
         elif event.key == pygame.K_q:
             sys.exit()
 
@@ -84,8 +86,18 @@ class AlienInvasion:
 
     def _check_play_button(self, mouse_pos):
         """Start a new game when the player clicks Play."""
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.game_active:
+            self.stats.reset_stats()
             self.game_active = True
+            
+            # Get rid of any remaining bullets and aliens.
+            self.bullets.empty()
+            self.aliens.empty()
+
+            # Create a new fleet and center the ship.
+            self._create_fleet()
+            self.ship.center_ship()
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ class Settings:
 
         # Ship settings
         self.ship_speed = 5
-        self.ship_limit = 3
+        self.ship_limit = 1 # base 3
 
         # Bullet settings
         self.bullet_speed = 5.0
